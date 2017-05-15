0ee803e67ed688c44f95fe4e18d4079e0496175b
Michael Morscher <ich@morschi.com>
2017-05-15 13:47:01 +0200

    Add new coaps example Makefile for CC2538

---
+++ examples/apps/coaps/main.c
@@ -0,0 +1,336 @@
+/*
+ *  Copyright (c) 2016, The OpenThread Authors.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef OPENTHREAD_CONFIG_FILE
+#include OPENTHREAD_CONFIG_FILE
+#else
+#include <openthread-config.h>
+#endif
+
+#include <assert.h>
+
+
+#include "openthread/udp.h"
+#include "openthread/platform/alarm.h"
+#include <openthread/cli.h>
+#include <openthread/diag.h>
+#include <openthread/platform/platform.h>
+#include <openthread/openthread.h>
+
+#include <string.h>
+#include <stdio.h>
+
+// Define TinyDTLS / YaCoAP
+#if OPENTHREAD_ENABLE_COAPS
+#define OPENTHREAD_ENABLE_UDPSERVER 1
+#define OPENTHREAD_ENABLE_YACOAP 1
+#define OPENTHREAD_ENABLE_TINYDTLS 1
+#endif
+
+/* TinyDTLS variables */
+#if OPENTHREAD_ENABLE_TINYDTLS
+#include "../third_party/tinydtls/dtls.h"
+#include "../third_party/tinydtls/dtls_debug.h"
+#define DTLS_LOG_LEVEL DTLS_LOG_WARN
+otInstance *mInstance;
+otSockAddr sockaddr;
+otUdpSocket mSocket;
+dtls_context_t *the_context = NULL;
+#endif
+
+/* YaCoAP variables */
+#if OPENTHREAD_ENABLE_TINYDTLS
+#include "../third_party/microcoap/coap.h"
+extern void resource_setup(const coap_resource_t *resources);
+extern coap_resource_t resources[];
+#endif
+
+#if OPENTHREAD_ENABLE_COAPS_CLI == 0
+#define otPlatLog(...)
+#endif
+
+#ifdef OPENTHREAD_MULTIPLE_INSTANCE
+void *otPlatCAlloc(size_t aNum, size_t aSize)
+{
+    return calloc(aNum, aSize);
+}
+
+void otPlatFree(void *aPtr)
+{
+    free(aPtr);
+}
+#endif
+
+void otTaskletsSignalPending(otInstance *aInstance)
+{
+    (void)aInstance;
+}
+
+#if defined(OPENTHREAD_ENABLE_TINYDTLS) && defined(DTLS_PSK)
+/* This function is the "key store" for tinyDTLS. It is called to
+ * retrieve a key for the given identity within this particular
+ * session. */
+static int get_psk_info(struct dtls_context_t *ctx, const session_t *session,
+						 dtls_credentials_type_t type,
+						 const unsigned char *id, size_t id_len,
+						 unsigned char *result, size_t result_length) {
+
+  struct keymap_t {
+    unsigned char *id;
+    size_t id_length;
+    unsigned char *key;
+    size_t key_length;
+  } psk[3] = {
+    { (unsigned char *)"Client_identity", 15,
+      (unsigned char *)"secretPSK", 9 },
+    { (unsigned char *)"default identity", 16,
+      (unsigned char *)"\x11\x22\x33", 3 },
+    { (unsigned char *)"\0", 2,
+      (unsigned char *)"", 1 }
+  };
+
+  if (type != DTLS_PSK_KEY) {
+    return 0;
+  }
+
+  if (id) {
+    uint8_t i;
+    for (i = 0; i < sizeof(psk)/sizeof(struct keymap_t); i++) {
+      if (id_len == psk[i].id_length && memcmp(id, psk[i].id, id_len) == 0) {
+	if (result_length < psk[i].key_length) {
+	  return dtls_alert_fatal_create(DTLS_ALERT_INTERNAL_ERROR);
+	}
+
+	memcpy(result, psk[i].key, psk[i].key_length);
+	return psk[i].key_length;
+      }
+    }
+  }
+  (void) session;
+  (void) ctx;
+  return dtls_alert_fatal_create(DTLS_ALERT_DECRYPT_ERROR);
+}
+#endif /* DTLS_PSK */
+
+#ifdef OPENTHREAD_ENABLE_TINYDTLS
+/* Sends a new OpenThread message to a given address */
+void send_message(struct dtls_context_t *ctx, session_t *session, uint8 *data, size_t len)
+{
+	otMessage *message;
+
+	// Create message and write payload
+	message = otUdpNewMessage(mInstance, true);
+	otMessageSetLength(message, len);
+	otMessageWrite(message, 0, data, len);
+
+	// Send packet to peer
+	otUdpSend(&mSocket, message, &session->messageInfo);
+	(void) ctx;
+}
+
+/* Handler that is called when a packet is received */
+int handle_read(struct dtls_context_t *context, session_t *session, uint8 *data, size_t length)
+{
+	#ifndef NDEBUG
+	char loggingBuffer[length];
+	snprintf(loggingBuffer, sizeof loggingBuffer, "%s", data);
+	otPlatLog(kLogLevelDebg, kLogRegionPlatform, "%d(MAIN): Received data (%d Byte)", otPlatAlarmGetNow(), length, loggingBuffer);
+	#endif
+
+	coap_packet_t requestPacket, responsePacket;
+	uint8_t responseBuffer[DTLS_MAX_BUF];
+	size_t responseBufferLength = sizeof(responseBuffer);
+
+	if ((coap_parse(data, length, &requestPacket)) < COAP_ERR)
+	{
+		// Get data from resources
+		coap_handle_request(resources, &requestPacket, &responsePacket);
+
+		// Build response packet
+		if ((coap_build(&responsePacket, responseBuffer, &responseBufferLength)) < COAP_ERR)
+		{
+			// Send response packet decrypted over DTLS
+			dtls_write(context, session, responseBuffer, responseBufferLength);
+		}
+	}
+	return 0;
+}
+
+/* Handler that is called when a packet should be sent */
+int handle_write(struct dtls_context_t *ctx, session_t *session, uint8 *data, size_t len)
+{
+	#ifndef NDEBUG
+	char buffer[len];
+	snprintf(buffer, sizeof buffer, "%s", data);
+	otPlatLog(kLogLevelDebg, kLogRegionPlatform, "%d(MAIN): Sending data (%d Byte)", otPlatAlarmGetNow(), len, buffer);
+	#endif
+
+	// Sending DTLS encrypted application data over UDP
+	send_message(ctx, session, data, len);
+
+	return len;
+}
+
+/* Handler that is called when an event occurs */
+int handle_event(struct dtls_context_t *ctx, session_t *session, dtls_alert_level_t level, unsigned short code)
+{
+	#ifndef NDEBUG
+	otPlatLog(kLogLevelDebg, kLogRegionPlatform, "%d(MAIN): Event occurred! (level %d, code %s )", otPlatAlarmGetNow(), level, code);
+	#endif
+
+	// Handle event here
+
+	(void) ctx;
+	(void) session;
+	(void) level;
+	(void) code;
+	return 0;
+}
+
+/* Definition of executed handlers */
+static dtls_handler_t dtls_callback = {
+  .write = handle_write,
+  .read  = handle_read,
+  .event = handle_event,
+#ifdef DTLS_PSK
+  .get_psk_info = get_psk_info,
+#endif
+};
+#endif
+
+#ifdef OPENTHREAD_ENABLE_UDPSERVER
+/* Handler that is called when a raw UDP packet is receiver*/
+void onUdpPacket(void *aContext, otMessage *aMessage, const otMessageInfo *aMessageInfo)
+{
+	// Get message payload
+    uint8_t payload[DTLS_MAX_BUF];
+    uint16_t payloadLength = otMessageGetLength(aMessage) - otMessageGetOffset(aMessage);
+    otMessageRead(aMessage, otMessageGetOffset(aMessage), payload, payloadLength);
+
+    // Set current session data
+    session_t session;
+    memset(&session, 0, sizeof(session_t));
+    session.size = sizeof(session.addr);
+    session.addr = aMessageInfo->mPeerAddr;
+    session.messageInfo = *aMessageInfo;
+
+    // Forward session and payload data to TinyDTLS
+    otPlatLog(kLogLevelDebg, kLogRegionPlatform, "%d(MAIN): Receiving data", otPlatAlarmGetNow());
+#ifdef OPENTHREAD_ENABLE_TINYDTLS
+    dtls_handle_message(the_context, &session, payload, payloadLength);
+#endif
+
+    (void) aContext;
+}
+#endif
+
+int main(int argc, char *argv[])
+{
+    otInstance *sInstance;
+
+#ifdef OPENTHREAD_MULTIPLE_INSTANCE
+    size_t otInstanceBufferLength = 0;
+    uint8_t *otInstanceBuffer = NULL;
+#endif
+
+    PlatformInit(argc, argv);
+
+#ifdef OPENTHREAD_MULTIPLE_INSTANCE
+    // Call to query the buffer size
+    (void)otInstanceInit(NULL, &otInstanceBufferLength);
+
+    // Call to allocate the buffer
+    otInstanceBuffer = (uint8_t *)malloc(otInstanceBufferLength);
+    assert(otInstanceBuffer);
+
+    // Initialize OpenThread with the buffer
+    sInstance = otInstanceInit(otInstanceBuffer, &otInstanceBufferLength);
+#else
+    sInstance = otInstanceInit();
+#endif
+    assert(sInstance);
+
+#if OPENTHREAD_ENABLE_COAPS_CLI
+    otCliUartInit(sInstance);
+#endif
+
+#if OPENTHREAD_ENABLE_DIAG
+    otDiagInit(sInstance);
+#endif
+
+    // Enable basic Thread config, ToDo: REMOVE THIS
+	otLinkSetPanId(sInstance, 0x1234);
+	otIp6SetEnabled(sInstance, true);
+	otThreadSetEnabled(sInstance, true);
+
+	// Add specific IP address, ToDo: REMOVE THIS
+	otNetifAddress aAddress;
+	otIp6AddressFromString("fdde:ad00:beef:0:5d12:76b8:948e:5b42", &aAddress.mAddress);
+	aAddress.mPrefixLength = 64;
+	aAddress.mPreferred = true;
+	aAddress.mValid = true;
+	otIp6AddUnicastAddress(sInstance, &aAddress);
+
+#if OPENTHREAD_ENABLE_UDPSERVER
+	// Create variables
+	memset(&mSocket, 0, sizeof(mSocket));
+	memset(&sockaddr, 0, sizeof(otSockAddr));
+	mInstance = sInstance;
+	sockaddr.mPort = 6666; // ToDo: hard coded
+
+	// Bind Port
+	otUdpOpen(sInstance, &mSocket, (otUdpReceive) &onUdpPacket, &mSocket);
+	otUdpBind(&mSocket, &sockaddr);
+#endif
+
+#if OPENTHREAD_ENABLE_TINYDTLS
+	// Initialise DTLS basics and setting log level
+	dtls_init();
+	dtls_set_log_level(DTLS_LOG_LEVEL);
+	the_context = dtls_new_context(&mSocket);
+	dtls_set_handler(the_context, &dtls_callback);
+#endif
+
+#if OPENTHREAD_ENABLE_YACOAP
+	// Initialise COAP resources
+	resource_setup(resources);
+#endif
+
+    while (1)
+    {
+        otTaskletsProcess(sInstance);
+        PlatformProcessDrivers(sInstance);
+    }
+
+    // otInstanceFinalize(sInstance);
+#ifdef OPENTHREAD_MULTIPLE_INSTANCE
+    // free(otInstanceBuffer);
+#endif
+
+    return 0;
+}
