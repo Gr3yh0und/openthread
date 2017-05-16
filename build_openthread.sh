db3b54e05397fd61a9ada6c83551818930de8e64
Michael Morscher <ich@morschi.com>
2017-05-16 19:32:24 +0200

    Improved buildscript

--- build_openthread.sh
+++ build_openthread.sh
@@ -4,17 +4,13 @@
 # Variables
 BINARYPATH="output/cc2538/bin"
 COMPILERPREFIX="ot"
+PLATFORM="cc2538"
 BINARYNAMEFTD="$COMPILERPREFIX-cli-ftd"
 BINARYNAMEMTD="$COMPILERPREFIX-cli-mtd"
 BINARYNAMENCPFTD="$COMPILERPREFIX-ncp-ftd"
 BINARYNAMENCPMTD="$COMPILERPREFIX-ncp-mtd"
-
-# Check platform arguments
-if [[  ($1 != "cc2538" && $1 != "cc2650") ]]; then 
-	echo "Error: Please supply cc2538 or cc2650 as an argument!"
-	exit
-fi
-PLATFORM=$1
+BINARYNAMECOAPSFTD="$COMPILERPREFIX-coaps-ftd"
+BINARYNAMECOAPSMTD="$COMPILERPREFIX-coaps-mtd"
 
 # Make project and check outcome
 echo "Entering build phase for target $PLATFORM ..."
@@ -46,6 +42,14 @@ if [ -f "$BINARYPATH/$BINARYNAMENCPMTD.bin" ]; then
 echo "$BINARYNAMENCPMTD.bin ...";
 	rm $BINARYPATH/$BINARYNAMENCPMTD.bin
 fi
+if [ -f "$BINARYPATH/$BINARYNAMECOAPSFTD.bin" ]; then
+echo "$BINARYNAMECOAPSFTD.bin ...";
+	rm $BINARYPATH/$BINARYNAMECOAPSFTD.bin
+fi
+if [ -f "$BINARYPATH/$BINARYNAMECOAPSMTD.bin" ]; then
+echo "$BINARYNAMECOAPSMTD.bin ...";
+	rm $BINARYPATH/$BINARYNAMECOAPSMTD.bin
+fi
 echo "Done!"
 echo ""
 
@@ -58,3 +62,36 @@ arm-none-eabi-objcopy -O binary $BINARYPATH/$BINARYNAMEFTD $BINARYPATH/$BINARYNA
 arm-none-eabi-objcopy -O binary $BINARYPATH/$BINARYNAMEMTD $BINARYPATH/$BINARYNAMEMTD.bin
 arm-none-eabi-objcopy -O binary $BINARYPATH/$BINARYNAMENCPFTD $BINARYPATH/$BINARYNAMENCPFTD.bin
 arm-none-eabi-objcopy -O binary $BINARYPATH/$BINARYNAMENCPMTD $BINARYPATH/$BINARYNAMENCPMTD.bin
+arm-none-eabi-objcopy -O binary $BINARYPATH/$BINARYNAMECOAPSFTD $BINARYPATH/$BINARYNAMECOAPSFTD.bin
+arm-none-eabi-objcopy -O binary $BINARYPATH/$BINARYNAMECOAPSMTD $BINARYPATH/$BINARYNAMECOAPSMTD.bin
+echo ""
+
+# Get size of new build
+OUTPUT_NEW="$(arm-none-eabi-size $BINARYPATH/$BINARYNAMECOAPSFTD)"
+RE_NEW="([0-9]{3,6})\s*([0-9]{3,6})\s*([0-9]{3,6})\s*([0-9]{3,6})"
+[[ $OUTPUT_NEW =~ $RE_NEW ]]
+	TEXT_NEW=${BASH_REMATCH[1]}
+	BSS_NEW=${BASH_REMATCH[2]}
+	DATA_NEW=${BASH_REMATCH[3]}
+	DEC_NEW=${BASH_REMATCH[4]}
+	
+# Get size of old build
+OUTPUT_OLD="$(cat build.old)"
+RE_OLD="([0-9]{3,6}),([0-9]{3,6}),([0-9]{3,6}),([0-9]{3,6})"
+[[ $OUTPUT_OLD =~ $RE_OLD ]]
+	TEXT_OLD=${BASH_REMATCH[1]}
+	BSS_OLD=${BASH_REMATCH[2]}
+	DATA_OLD=${BASH_REMATCH[3]}
+	DEC_OLD=${BASH_REMATCH[4]}
+
+# Calculate differences
+TEXT_DIF=`expr $TEXT_NEW - $TEXT_OLD`
+BSS_DIF=`expr $BSS_NEW - $BSS_OLD`
+DATA_DIF=`expr $DATA_NEW - $DATA_OLD`
+DEC_DIF=`expr $DEC_NEW - $DEC_OLD`
+
+# Print results
+echo "Old: TEXT($TEXT_OLD), BSS($BSS_OLD), DATA($DATA_OLD), DEC($DEC_OLD)"
+echo "New: TEXT($TEXT_NEW), BSS($BSS_NEW), DATA($DATA_NEW), DEC($DEC_NEW)"
+echo "Dif: TEXT($TEXT_DIF), BSS($BSS_DIF), DATA($DATA_DIF), DEC($DEC_DIF)"
+echo "$TEXT_NEW,$BSS_NEW,$DATA_NEW,$DEC_NEW" > build.old
